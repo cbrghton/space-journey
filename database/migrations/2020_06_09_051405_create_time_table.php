51aec24d84fcde5d354fffe3fce25ccae8c701ce
Brighton <cbrghton@gmail.com>
2020-06-09 01:15:10 -0500

    Modify time migration, add nullable option for columns end activity, start break, end break and total hours

--- database/migrations/2020_06_09_051405_create_time_table.php
+++ database/migrations/2020_06_09_051405_create_time_table.php
@@ -22,13 +22,17 @@ class CreateTimeTable extends Migration
             $table->timestampTz('start_activity')
                 ->comment('Real start timestamp of activity');
             $table->timestampTz('end_activity')
-                ->comment('Real end timestamp of activity');
+                ->comment('Real end timestamp of activity')
+                ->nullable();
             $table->timestampTz('start_break')
-                ->comment('Real start timestamp of break time');
+                ->comment('Real start timestamp of break time')
+                ->nullable();
             $table->timestampTz('end_break')
-                ->comment('Real end timestamp of break time');
+                ->comment('Real end timestamp of break time')
+                ->nullable();
             $table->timeTz('total_hours')
-                ->comment('Total hours of activity day');
+                ->comment('Total hours of activity day')
+                ->nullable();
             $table->timestamps();
             $table->softDeletesTz();
         });
